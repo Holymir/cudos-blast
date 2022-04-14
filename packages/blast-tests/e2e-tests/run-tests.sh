0610e8c673d38afe32bd3b3facea7d660e8b500e
Ventsi <ventsi@limechain.tech>
2022-04-14 17:30:01 +0300

    error case

--- packages/blast-tests/e2e-tests/run-tests.sh
+++ packages/blast-tests/e2e-tests/run-tests.sh
@@ -4,7 +4,7 @@ source ./packages/blast-tests/e2e-tests/vars.sh
 echo '- Executing node-start-status.test.sh...'
 $TESTS_FOLDER/node.test.sh
 
-if [[ $? == 0 ]]; then
+if [[ $? != 0 ]]; then
     echo ERROR!!
     exit_status=1
 else
